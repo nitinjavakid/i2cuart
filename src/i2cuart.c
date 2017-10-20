2f2aaeaf9e509319d5ba6bc7192dcb9fd93b0efd
Nitin Anand <nitinjavakid@gmail.com>
2017-10-20 11:06:47 +0530

    Changes to support debug logging

--- src/i2cuart.c
+++ src/i2cuart.c
@@ -8,6 +8,7 @@ int main()
     n_usart_enable(N_USART_8BIT, N_USART_PARITY_NONE, N_USART_STOPBIT1, 115200);
     twi_handle = n_twi_new_slave_io(0x04, 100);
     usart_handle = n_usart_new_io(100);
+    n_debug_init(usart_handle);
 
     while(1)
     {
